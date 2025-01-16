4550da4709f6458c2e6251e8d067aa1274dab260
nicolo_canevari <nico.canevari@outlook.it>
2025-01-16 14:55:41 +0100

    [FEAT] JS: array di oggetti + console.table

---
+++ SNACK-1/JS/script.js
@@ -0,0 +1,11 @@
+// Creiamo un array di oggetti con le poprietà delle bici
+let bici = [
+
+    { nome: "Bici: Bianchi", peso: 8.5 },
+    { nome: "Bici: Wilier Triestina", peso: 7.2 },
+    { nome: "Bici: Colnago", peso: 6.9 },
+    { nome: "Bici: De Rosa", peso: 9.1 }
+
+  ];
+
+  console.table(bici)
