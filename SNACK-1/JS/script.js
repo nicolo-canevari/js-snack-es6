4459ed76be1ae4a597ae1f5c996b4f3b048ee2e4
nicolo_canevari <nico.canevari@outlook.it>
2025-01-16 15:24:15 +0100

    [FEAT] JS: array squadre + console.table

--- SNACK-1/JS/script.js
+++ SNACK-1/JS/script.js
@@ -6,23 +6,23 @@ let bici = [
     { nome: "Bici Colnago", peso: 6.9 },
     { nome: "Bici De Rosa", peso: 9.1 }
 
-  ];
+];
 
-  console.table(bici)
+console.table(bici)
 
-   // Inizializziamo la bici più leggera come il primo oggetto dell'array
-   let biciLeggera = bici[0];
+// Inizializziamo la bici più leggera come il primo oggetto dell'array
+let biciLeggera = bici[0];
 
-   // Ciclo for sull'array per trovare la bici con il peso minore
-   for (let i = 1; i < bici.length; i++) {
+// Ciclo for sull'array per trovare la bici con il peso minore
+for (let i = 1; i < bici.length; i++) {
 
     if (bici[i].peso < biciLeggera.peso) {
 
-      biciLeggera = bici[i];
+        biciLeggera = bici[i];
 
     }
 
-  }
+}
 
-  // Stampiamo a schermo la bici con il peso minore
+// Stampiamo a schermo la bici con il peso minore
 console.log(`La bici con il peso minore è: ${biciLeggera.nome} con un peso di ${biciLeggera.peso} kg.`);
---
+++ SNACK-2/JS/script.js
@@ -0,0 +1,12 @@
+// Array con i nomi delle squadre
+const squadre = [
+
+    { nome: "Juventus", punti: 0, falliSubiti: 0 },
+    { nome: "Inter", punti: 0, falliSubiti: 0 },
+    { nome: "Milan", punti: 0, falliSubiti: 0 },
+    { nome: "Roma", punti: 0, falliSubiti: 0 },
+    { nome: "Napoli", punti: 0, falliSubiti: 0 }
+
+];
+
+console.table(squadre)
