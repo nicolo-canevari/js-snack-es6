6c7a00e826cc2b179a4a153fc162c3236ac471fc
nicolo_canevari <nico.canevari@outlook.it>
2025-01-16 15:38:02 +0100

    [FEAT] JS: Assegnazione numeri random alle proprietà "punti" e "falliSubiti"

--- SNACK-2/JS/script.js
+++ SNACK-2/JS/script.js
@@ -5,8 +5,16 @@ const squadre = [
     { nome: "Inter", punti: 0, falliSubiti: 0 },
     { nome: "Milan", punti: 0, falliSubiti: 0 },
     { nome: "Roma", punti: 0, falliSubiti: 0 },
-    { nome: "Napoli", punti: 0, falliSubiti: 0 }
+    { nome: "Napoli", punti: 0, falliSubiti: 0 }   
 
 ];
 
 console.table(squadre)
+
+// Assegnazione numeri random alle proprietà "punti" e "falliSubiti"
+squadre.forEach(squadra => {
+
+    squadra.punti = Math.floor(Math.random() * 100); // Numero random tra 0 e 99
+    squadra.falliSubiti = Math.floor(Math.random() * 50); // Numero random tra 0 e 49
+
+  });
