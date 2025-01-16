8a2342e2b2441598cb74bc71bda92e50d6f37aac
nicolo_canevari <nico.canevari@outlook.it>
2025-01-16 15:54:43 +0100

    [FEAT] JS: aggiunta nuovo array (.map) + stampo in console

--- SNACK-2/JS/script.js
+++ SNACK-2/JS/script.js
@@ -5,11 +5,11 @@ const squadre = [
     { nome: "Inter", punti: 0, falliSubiti: 0 },
     { nome: "Milan", punti: 0, falliSubiti: 0 },
     { nome: "Roma", punti: 0, falliSubiti: 0 },
-    { nome: "Napoli", punti: 0, falliSubiti: 0 }   
+    { nome: "Napoli", punti: 0, falliSubiti: 0 }
 
 ];
 
-console.table(squadre)
+// console.table(squadre)
 
 // Assegnazione numeri random alle proprietà "punti" e "falliSubiti"
 squadre.forEach(squadra => {
@@ -17,4 +17,17 @@ squadre.forEach(squadra => {
     squadra.punti = Math.floor(Math.random() * 100); // Numero random tra 0 e 99
     squadra.falliSubiti = Math.floor(Math.random() * 50); // Numero random tra 0 e 49
 
-  });
+});
+
+// Nuovo array con solo i nomi e i falli subiti
+// Uso .map per creare nuovo array a partire da uno esistente, ma con un formato diverso o con alcune modifiche ai dati
+const squadreFalliSubiti = squadre.map(squadra => ({
+
+  nome: squadra.nome,
+  falliSubiti: squadra.falliSubiti
+
+}));
+
+// Stampo entrambi gli array in console usando console.table
+console.table(squadre);
+console.table(squadreFalliSubiti);
